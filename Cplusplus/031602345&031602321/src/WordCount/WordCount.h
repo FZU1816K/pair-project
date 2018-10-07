a3a31ed5d57791457063623a137f80717ede0833
QiuSun <1259125423@qq.com>
2018-10-07 14:55:25 +0800

    第一次提交src部分

---
+++ Cplusplus/031602345&031602321/src/WordCount/WordCount.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <assert.h>
+
+using namespace std;
+
+// 解析参数
+void ParserParam(int argc, char *argv[]);
+
+// 自定义getline函数 返回值为之后是否还有字符
+bool GetLine(ifstream &instream, string &line);
+
+// 字符统计
+int CountChar(string input_path);
+
+// 有效行数统计
+int CountLine(string input_path);
+
+// 有效单词总数统计
+int CountWord(string input_path);
+
+// 得到词频统计
+void GetWordCountMap(string input_path);
+
+// return a orderd vector of WordNode
+vector<map<string, int>::iterator> GetFirstNWords(string file_location);
+
+// 展示结果
+void ShowResult(int characters, int words, int lines, vector<map<string, int>::iterator> first_n_words, string ouput_path);
