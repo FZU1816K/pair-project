ac045ff7bf9c96a1e476840ce2366600c559c26c
Stellaaa18 <18120860780@163.com>
2018-10-09 18:00:09 +0800

    feat: analyse parameter

--- Cplusplus/181600215&&061600236/src/WordCount/AnalyzeArguments.cpp
+++ Cplusplus/181600215&&061600236/src/WordCount/AnalyzeArguments.cpp
@@ -1,44 +1,48 @@
 #include "stdafx.h"
 #include "AnalyzeArguments.h"
 
-string inputFile="input7.txt";
-string outputFile="rsult.txt";
-int phraseLen=3;
-int topN=10;
-bool weightOnWord=0;
+string inputFile="input.txt";
+string outputFile="result.txt";
+bool weightOnWord=1;
+int phraseLen = 3;
+int topN = 10;
+bool _m=0;
 
 int AnalyseParameter(char **argv)
 {
+	bool _i=0, _o=0, _w = 0;
 	int id = 1;
 	while (argv[id] != NULL) {
-		cout << id<<" "<<argv[id] << endl;
 		if (strcmp(argv[id], "-i") == 0) {
 			if (argv[id + 1] != NULL) {
 				inputFile = argv[id + 1];
+				_i = 1;
 			}
 			else {
 				cout << "parameter error." << endl;
-				return -1;
+				return 0;
 			}
 			id += 2;
 		}
 		else if (strcmp(argv[id], "-o") == 0) {
 			if (argv[id + 1] != NULL) {
 				outputFile = argv[id + 1];
+				_o = 1;
 			}
 			else {
 				cout << "parameter error." << endl;
-				return -1;
+				return 0;
 			}
 			id += 2;
 		}
 		else if (strcmp(argv[id], "-m") == 0) {
+			_m = 1;
 			if (argv[id + 1] != NULL) {
 				phraseLen = atoi(argv[id + 1]);
 			}
 			else {
 				cout << "parameter error." << endl;
-				return -1;
+				return 0;
 			}
 			id += 2;
 		}
@@ -48,30 +52,27 @@ int AnalyseParameter(char **argv)
 			}
 			else {
 				cout << "parameter error." << endl;
-				return -1;
+				return 0;
 			}
 			id += 2;
 		}
 		else if (strcmp(argv[id], "-w") == 0) {
 			if (argv[id + 1] != NULL) {
-				if (atoi(argv[id + 1]) == 1) {
-					weightOnWord = 1;
-				}
+				weightOnWord = atoi(argv[id + 1]);
+				_w = 1;
 			}
 			else {
 				cout << "parameter error." << endl;
-				return -1;
+				return 0;
 			}
 			id += 2;
 		}
 		else {
 			cout << "parameter error." << endl;
-			return -1;
+			return 0;
 		}
-
-		return 1;
 	}
-	
-	return 1;
+
+	return (_i && _o && _w);
 }
 
